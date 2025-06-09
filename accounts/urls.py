ef86405d065a88c0a151fc36c2152eca09ecd088
K B  BHAGAVAL KRISHNA <bhagavalkrishna@gmail.com>
2025-06-10 01:07:51 +0530

    add register_view, login_view, details_view, profile_view and created templates for all these views

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,15 +4,16 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    # Root URL - redirect to profile
+    path('', views.ProfileView.as_view(), name='index'),
+    
     # Authentication URLs
     path('register/', views.register_view, name='register'), 
     path('login/', views.login_view, name='login'),
     
-    # Details
-    path('user/details/', views.details_view, name='details'), ## details page 
     # Profile Management URLs
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('details/', views.details_view, name='details'),
     path('profile/fitness-goals/', views.fitness_goals_view, name='fitness_goals'),
     path('profile/activity-history/', views.activity_history_view, name='activity_history'),
-    path('profile/edit/', views.edit_profile_view, name='edit_profile'),
 ] 
